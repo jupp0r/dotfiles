75768e324eede1fb6ed33cb67e22a8dae14a8a33
Jupp Müller <jupp0r@gmail.com>
2012-02-28 18:08:07 +0100

    moved back to vanilla prelude, updated oh-my-zsh, misc emacs customizations

--- emacs/jupp.el
+++ emacs/jupp.el
@@ -1,26 +1,21 @@
-; disable autosave
+;; disable autosave
 (setq make-backup-files nil)
 
-; stop creating those backup~ files
+;; stop creating those backup~ files
 (setq make-backup-files nil)
-; stop creating those #auto-save# files
+;; stop creating those #auto-save# files
 (setq auto-save-default nil)
 
-; allow for German umlauts via Alt-U <letter>
+;; allow for German umlauts via Alt-U <letter>
 (setq mac-option-modifier nil)
 (setq mac-function-modifier 'meta)
-; I'm on the tab side of the force
-; http://www.jwz.org/doc/tabs-vs-spaces.html for details
-(setq indent-tabs-mode t)
-(setq c-basic-offset 2)
-(setq tab-width 2)
 
-; erlang mode
-(let ((erlang-emacs-dir "/usr/local/Cellar/erlang/R14B04/lib/erlang/lib/tools-2.6.6.5/emacs"))
+;; erlang mode
+(let ((erlang-emacs-dir "/usr/local/Cellar/erlang/R15B/lib/erlang/lib/tools-2.6.6.6/emacs"))
     (when (file-exists-p erlang-emacs-dir)
             (add-to-list 'load-path erlang-emacs-dir)
-            (setq erlang-root-dir "/usr/local/Cellar/erlang/R14B04")
-            (setq exec-path (cons "/usr/local/Cellar/erlang/R14B04/bin" exec-path))
+            (setq erlang-root-dir "/usr/local/Cellar/erlang/R15B")
+            (setq exec-path (cons "/usr/local/Cellar/erlang/R15B/bin" exec-path))
             (require 'erlang-start)
             (require 'erlang-eunit)
             (add-hook 'erlang-mode-hook
@@ -31,62 +26,64 @@
                                            (save-buffer)
                                            (erlang-eunit-compile-and-run-module-tests)))))))
 
-; cursor as bar
-(add-to-list 'default-frame-alist '(cursor-type . box))
+;; restore arrow key navigation
+(prelude-restore-arrow-keys)
 
-; rsense
+;; get terminal colors for cmake builds
+(add-to-list 'compilation-environment "EMACS=n")
+
+;; scroll to compilation buffer bottom
+(setq-default compilation-scroll-output t)
+
+;; cursor as bar
+(add-to-list 'default-frame-alist '(cursor-type . bar))
+
+;; rsense
 (let ((rsense-home-dir "/usr/local/Cellar/rsense/0.3/libexec"))
     (when (file-exists-p rsense-home-dir)
         (setq rsense-home rsense-home-dir)
         (add-to-list 'load-path (concat rsense-home-dir "/etc"))
         (require 'rsense)))
 
-; auto-complete-clang
-(let ((auto-complete-dir (concat prelude-vendor-dir "/auto-complete")))
-    (setq ac-dictionary-directories ())
-    (add-to-list 'ac-dictionary-directories (concat auto-complete-dir "/dict"))
-    (require 'auto-complete-config)
-    (ac-config-default))
-
-; clang autocomplete
-(let ((auto-complete-clang-dir (concat prelude-vendor-dir "/auto-complete-clang")))
-    (add-to-list 'load-path auto-complete-clang-dir)
-    (require 'auto-complete-clang))
-
-; tab stops
+;; tab stops
 (setq-default tab-width 4)
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
 (setq-default ruby-basic-offset 2)
 
-; start edit server, allows to edit stuff in browser in emacs
-(require 'edit-server)
-(edit-server-start)
+(setq safe-local-variable-values
+      (quote
+       ((ruby-compilation-executable . "ruby")
+        (ruby-compilation-executable . "ruby1.8")
+        (ruby-compilation-executable . "ruby1.9")
+        (ruby-compilation-executable . "rbx")
+        (ruby-compilation-executable . "jruby")
+        (compile-command . "cd ../build && cmake -Denable_testing=ON -DCMAKE_BUILD_TYPE:STRING=Debug .. && make && tests/alltests --gtest_color=yes 2> /dev/null"))))
 
 ;; don't ask for compilation command
 (setq compilation-read-command nil)
 
 ;; compile key, maybe a little dangerous so close to quit
-(global-set-key (kbd "C-c c") 'compile)
+(defun c-w-c ()
+  (interactive)
+  (setq current-prefix-arg '(4))
+  (call-interactively 'compile))
+(global-set-key (kbd "C-c c") 'c-w-c)
 
 ;; no sound, but visible bell
 (setq-default visible-bell t)
 
 (prefer-coding-system 'utf-8)
 
-(setq font-lock-unfontify-region-function 'ansi-color-unfontify-region)
+;;(setq font-lock-unfontify-region-function 'ansi-color-unfontify-region)
 
 ;; workgroups
-;; this is a really ugly fix for a broken el-get recipe for workgroups
-(let ((workgroup-install-dir "/Users/jupp/.emacs.d/el-get/workgroups")
-      (workgroup-custom-setting-file "/Users/jupp/.workgroups/beleg"))
-  (when (file-exists-p workgroup-install-dir)
-    (add-to-list 'load-path workgroup-install-dir)
-    (require 'workgroups)
-    (setq wg-prefix-key (kbd "C-z"))
-    (workgroups-mode 1)
-    (when (file-exists-p workgroup-custom-setting-file)
-      (wg-load workgroup-custom-setting-file))))
+(require 'workgroups)
+(let ((workgroup-custom-setting-file "/Users/jupp/.workgroups/beleg"))
+  (setq wg-prefix-key (kbd "C-z"))
+  (workgroups-mode 1)
+  (when (file-exists-p workgroup-custom-setting-file)
+    (wg-load workgroup-custom-setting-file)))
 
 ;; I use google C coding style
 (add-hook 'c-mode-common-hook 'google-set-c-style)
@@ -102,5 +99,5 @@
 (scroll-bar-mode -1)
 
 ;; smoother scrolling
- (setq scroll-step           1
-       scroll-conservatively 10000)
+(setq scroll-step           1
+      scroll-conservatively 10000)
